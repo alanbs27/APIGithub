13e2d27a62964604945f7215260cfb57c4c3c1da
ALAN BRITO DA SILVA <alanbs27@gmail.com>
2023-04-12 13:23:23 -0300

    Update apigit.py
    adicionado comentarios

--- apigit.py
+++ apigit.py
@@ -12,11 +12,7 @@ print(response_dict.keys())
 
 print('Total repositories:', response_dict['total_count'])
 repo_dicts = response_dict['items']
-#print('Repositories returned:', len(repo_dicts))
-#repo_dict = repo_dicts[0]
-# print('\nKeys:', len(repo_dict))
-# for key in sorted(repo_dict.keys()):
-#     print(key)
+
 nome = []
 proprietario = []
 estrela = []
@@ -34,10 +30,12 @@ for repo_dict in repo_dicts:
      criado.append(repo_dict['created_at'])
      atualizado.append(repo_dict['updated_at'])
      descricao.append(repo_dict['description'])
+
 #df = pd.DataFrame.from_dict(repo_dicts)
 df = pd.DataFrame({'nomes': nome, 'proprietarios': proprietario, 'estrelas':estrela, 'caminhos': caminho,
                        'criados': criado, 'atualizado': atualizado, 'descricaos':descricao})
 
+#Salva os dados em dataframe como arquivo excel
 df.to_excel('resultado.xlsx', index=False)
 
 
